43d6f0a2d37c8e3bc3fa05896d0d59c86dcb2a44
dnjswlekd <wj8014@gmail.com>
2025-03-02 21:05:15 +0900

    visual animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// 1. 스플리팅 호출
 $(function () {
   Splitting();
+});
+
+// 2. 헤더 영역 스크롤 방향 이벤트
+$(function () {
+  var prevScrollTop = 0;
+  document.addEventListener('scroll', function () {
+    var nowScrollTop = $(window).scrollTop();
+
+    if (nowScrollTop > prevScrollTop) {
+      $('header').addClass('active');
+    } else {
+      $('header').removeClass('active');
+    }
+    prevScrollTop = nowScrollTop;
+  });
+});
+
+// 3. scrolla.js
+$(function () {
+  $('.animate').scrolla({
+    mobile: true,
+    once: false,
+  });
+});
+
+//4. svg path 길이 구하기
+$(function () {
+  $('.svg-ani')
+    .find('#svgAni01')
+    .each(function (i, path) {
+      var length = path.getTotalLength();
+    });
 });
